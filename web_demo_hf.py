dc089201ea6005eb7ac797aabbb9b474567cea80
tuteng0915 <leotuteng@126.com>
2023-05-19 18:07:20 +0800

    update web_demo_hf

--- web_demo_hf.py
+++ web_demo_hf.py
@@ -58,7 +58,7 @@ def parse_text(text):
 
 def predict(input, image_path, chatbot, max_length, top_p, temperature, history):
     if image_path is None:
-        return [(input, "图片为空！请重新上传图片并重试。")]
+        return [(input, "图片不能为空。请重新上传图片并重试。")], []
     chatbot.append((parse_text(input), ""))
     for response, history in model.stream_chat(tokenizer, image_path, input, history, max_length=max_length, top_p=top_p,
                                                temperature=temperature):
@@ -86,35 +86,45 @@ def reset_state():
     return None, [], []
 
 
-with gr.Blocks() as demo:
-    gr.HTML("""<h1 align="center">VisualGLM</h1>""")
+DESCRIPTION = '''<h1 align="center"><a href="https://github.com/THUDM/VisualGLM-6B">VisualGLM</a></h1>'''
+MAINTENANCE_NOTICE = 'Hint 1: If the app report "Something went wrong, connection error out", please turn off your proxy and retry.\nHint 2: If you upload a large size of image like 10MB, it may take some time to upload and process. Please be patient and wait.'
+NOTES = 'This app is adapted from <a href="https://github.com/THUDM/VisualGLM-6B">https://github.com/THUDM/VisualGLM-6B</a>. It would be recommended to check out the repo if you want to see the detail of our model and training process.'
 
-    image_path = gr.Image(type="filepath", label="Image Prompt", value=None)
-    chatbot = gr.Chatbot()
+
+with gr.Blocks(css='style.css') as demo:
+    gr.HTML(DESCRIPTION)
+    
     with gr.Row():
+        with gr.Column(scale=2):
+            image_path = gr.Image(type="filepath", label="Image Prompt", value=None).style(height=512)
         with gr.Column(scale=4):
-            with gr.Column(scale=12):
-                user_input = gr.Textbox(show_label=False, placeholder="Input...", lines=10).style(
-                    container=False)
-            with gr.Column(min_width=32, scale=1):
-                submitBtn = gr.Button("Submit", variant="primary")
-        with gr.Column(scale=1):
-            emptyBtn = gr.Button("Clear History")
+            chatbot = gr.Chatbot().style(height=512)
+    with gr.Row():
+        with gr.Column(scale=2, min_width=100):
             max_length = gr.Slider(0, 4096, value=2048, step=1.0, label="Maximum length", interactive=True)
             top_p = gr.Slider(0, 1, value=0.4, step=0.01, label="Top P", interactive=True)
             temperature = gr.Slider(0, 1, value=0.8, step=0.01, label="Temperature", interactive=True)
-
+        with gr.Column(scale=4):
+            with gr.Box():
+                with gr.Row():
+                    with gr.Column(scale=2):
+                        user_input = gr.Textbox(show_label=False, placeholder="Input...", lines=4).style(
+                            container=False)
+                    with gr.Column(scale=1, min_width=64):
+                        submitBtn = gr.Button("Submit", variant="primary")
+                        emptyBtn = gr.Button("Clear History")
+                gr.Markdown(MAINTENANCE_NOTICE + '\n' + NOTES)
     history = gr.State([])
+    
 
     submitBtn.click(predict, [user_input, image_path, chatbot, max_length, top_p, temperature, history], [chatbot, history],
                     show_progress=True)
-
     image_path.upload(predict_new_image, [image_path, chatbot, max_length, top_p, temperature], [chatbot, history],
                       show_progress=True)
     image_path.clear(reset_state, outputs=[image_path, chatbot, history], show_progress=True)
-
     submitBtn.click(reset_user_input, [], [user_input])
-
     emptyBtn.click(reset_state, outputs=[image_path, chatbot, history], show_progress=True)
 
-demo.queue().launch(share=False, inbrowser=True)
+    print(gr.__version__)
+
+demo.queue().launch(share=False, inbrowser=True, server_name='0.0.0.0', server_port=8080)
