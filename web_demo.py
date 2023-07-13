fbb6c5469bedfb9705703956748db1a8f7bd95ff
90houlaoheshang <907333918@qq.com>
2023-07-13 13:04:47 +0800

    格式化代码结构

--- web_demo.py
+++ web_demo.py
@@ -7,6 +7,7 @@ import json
 from model import is_chinese, get_infer_setting, generate_input, chat
 import torch
 
+
 def generate_text_with_image(input_text, image, history=[], request_data=dict(), is_zh=True):
     input_para = {
         "max_length": 2048,
@@ -19,17 +20,17 @@ def generate_text_with_image(input_text, image, history=[], request_data=dict(),
     input_para.update(request_data)
 
     input_data = generate_input(input_text, image, history, input_para, image_is_encoded=False)
-    input_image, gen_kwargs =  input_data['input_image'], input_data['gen_kwargs']
+    input_image, gen_kwargs = input_data['input_image'], input_data['gen_kwargs']
     with torch.no_grad():
         answer, history, _ = chat(None, model, tokenizer, input_text, history=history, image=input_image, \
-                            max_length=gen_kwargs['max_length'], top_p=gen_kwargs['top_p'], \
-                            top_k = gen_kwargs['top_k'], temperature=gen_kwargs['temperature'], english=not is_zh)
+                                  max_length=gen_kwargs['max_length'], top_p=gen_kwargs['top_p'], \
+                                  top_k=gen_kwargs['top_k'], temperature=gen_kwargs['temperature'], english=not is_zh)
     return answer
 
 
 def request_model(input_text, temperature, top_p, image_prompt, result_previous):
     result_text = [(ele[0], ele[1]) for ele in result_previous]
-    for i in range(len(result_text)-1, -1, -1):
+    for i in range(len(result_text) - 1, -1, -1):
         if result_text[i][0] == "" or result_text[i][1] == "":
             del result_text[i]
     print(f"history {result_text}")
@@ -43,7 +44,7 @@ def request_model(input_text, temperature, top_p, image_prompt, result_previous)
         return input_text, result_text
     elif input_text == "":
         result_text.append((input_text, 'Text empty! Please enter text and retry.'))
-        return "", result_text                
+        return "", result_text
 
     request_para = {"temperature": temperature, "top_p": top_p}
     image = Image.open(image_prompt)
@@ -73,6 +74,7 @@ NOTES = 'This app is adapted from <a href="https://github.com/THUDM/VisualGLM-6B
 def clear_fn(value):
     return "", [("", "Hi, What do you want to know about this image?")], None
 
+
 def clear_fn2(value):
     return [("", "Hi, What do you want to know about this image?")]
 
@@ -81,13 +83,14 @@ def main(args):
     gr.close_all()
     global model, tokenizer
     model, tokenizer = get_infer_setting(gpu_device=0, quant=args.quant)
-    
+
     with gr.Blocks(css='style.css') as demo:
         gr.Markdown(DESCRIPTION)
         with gr.Row():
             with gr.Column(scale=4.5):
                 with gr.Group():
-                    input_text = gr.Textbox(label='Input Text', placeholder='Please enter text prompt below and press ENTER.')
+                    input_text = gr.Textbox(label='Input Text',
+                                            placeholder='Please enter text prompt below and press ENTER.')
                     with gr.Row():
                         run_button = gr.Button('Generate')
                         clear_button = gr.Button('Clear')
@@ -100,15 +103,16 @@ def main(args):
                     with gr.Row():
                         maintenance_notice = gr.Markdown(MAINTENANCE_NOTICE1)
             with gr.Column(scale=5.5):
-                result_text = gr.components.Chatbot(label='Multi-round conversation History', value=[("", "Hi, What do you want to know about this image?")]).style(height=550)
+                result_text = gr.components.Chatbot(label='Multi-round conversation History', value=[
+                    ("", "Hi, What do you want to know about this image?")]).style(height=550)
 
         gr.Markdown(NOTES)
 
         print(gr.__version__)
-        run_button.click(fn=request_model,inputs=[input_text, temperature, top_p, image_prompt, result_text],
+        run_button.click(fn=request_model, inputs=[input_text, temperature, top_p, image_prompt, result_text],
                          outputs=[input_text, result_text])
-        input_text.submit(fn=request_model,inputs=[input_text, temperature, top_p, image_prompt, result_text],
-                         outputs=[input_text, result_text])
+        input_text.submit(fn=request_model, inputs=[input_text, temperature, top_p, image_prompt, result_text],
+                          outputs=[input_text, result_text])
         clear_button.click(fn=clear_fn, inputs=clear_button, outputs=[input_text, result_text, image_prompt])
         image_prompt.upload(fn=clear_fn2, inputs=clear_button, outputs=[result_text])
         image_prompt.clear(fn=clear_fn2, inputs=clear_button, outputs=[result_text])
@@ -116,11 +120,12 @@ def main(args):
         print(gr.__version__)
 
     demo.queue(concurrency_count=10)
-    demo.launch(share=args.share,server_name=args.server_name)
+    demo.launch(share=args.share, server_name=args.server_name)
 
 
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--quant", choices=[8, 4], type=int, default=None)
     parser.add_argument("--share", action="store_true")
